2d0e4357ce3dc1a217faf8ca9d55604c240ec9bd
saiful2612 <bristykodom@gmail.com>
2021-04-04 11:46:50 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { useState } from 'react';
+
+
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+
+  const [user, setUser]=useState({
+    isSignedIn : false,
+    name: '',
+    Email: ''
+  })
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  const handleClick = () =>{
+    firebase.auth().signInWithPopup(provider)
+    .then(res =>{
+      const {displayName, email} =res.user;
+      const signedInUser={
+        isSignedIn : true,
+        name: displayName,
+        Email: email
+      }
+      setUser(signedInUser);
+    })
+    .catch(err => {
+      console.log(err);
+      console.log(err.message)
+    })
+  }
+
+  const handleSignOut = () => {
+    firebase.auth().signOut()
+    .then(res => {
+      const signOutUser= {
+        isSignedIn: false,
+        name: '',
+        Email: ''
+      }
+      setUser(signOutUser)
+    })
+    .catch(err => {
+
+    })
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>This is pratice part</h2>
+      <Button variant="contained" color="primary">
+        Hello World
+    </Button>
+
+    {
+      user.isSignedIn ? <button onClick={handleSignOut}>Sign Out</button> :
+      <button onClick={handleClick}>Sign In</button>
+    }
+    {
+      user.isSignedIn && <p> Welcome, {user.name} </p>
+    }
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDCDhLS2slB3uAnuA4DLHvO5wH9kYqlmaU",
+    authDomain: "amader-pathshala.firebaseapp.com",
+    projectId: "amader-pathshala",
+    storageBucket: "amader-pathshala.appspot.com",
+    messagingSenderId: "660603398751",
+    appId: "1:660603398751:web:4e9d33bd004374ec8109ea"
+
+}; 
+
+
+export default firebaseConfig;
